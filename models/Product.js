beb584cd90f333cf7ff680f2a36a97fcb93f1544
altavada <117209509+altavada@users.noreply.github.com>
2023-02-27 13:50:42 -0500

    Created models and data relationships

--- models/Product.js
+++ models/Product.js
@@ -9,7 +9,22 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    product_name: {
+      type: DataTypes.STRING
+    },
+    price: {
+      type: DataTypes.DECIMAL(5,2)
+    },
+    stock: {
+      type: DataTypes.INTEGER
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'category',
+        key: 'id'
+      }
+    }
   },
   {
     sequelize,
